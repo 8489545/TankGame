79a23c8b46c97b64ef99acbaf7a669522afff213
8489545 <rebob9545@gmail.com>
2020-01-31 15:52:12 +0900

    폭격탱크 완성

---
+++ Space/Bomb.h
@@ -0,0 +1,19 @@
+#pragma once
+class Bomb : public Object
+{
+	Sprite* m_Bomb;
+public:
+	Bomb(Vec2 Pos);
+	~Bomb();
+
+	Vec2 Vertex1, Vertex2, Vertex3, Vertex4;
+
+	float vy;
+
+	void Update(float deltaTime, float time);
+	void Render();
+	void OnCollision(Object* other);
+
+	void SetVertex();
+};
+
